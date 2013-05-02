1ce52f5c94b55aa98a2d4c0661230d2f593851d5
Phil Thompson <hello@philthompson.co.uk>
2013-05-02 14:57:16 +0100

    JSLint ran on a JS file and improvements made from that feedback

--- site/behaviour/ajax_pagination.js
+++ site/behaviour/ajax_pagination.js
@@ -3,16 +3,15 @@
  *	turn normal HTML pagination into AJAX so only the content
  *	affected by the pagination is updated.
  * 
- *	@copyright 	2009-2013 (c)	Phil Thompson	http://philthompson.co.uk
+ *	@copyright	2009-2013	Phil Thompson	http://philthompson.co.uk
  *	@license
  *	@version	1.1	
  *	@author		philthompson.co.uk
  *	@since		22/04/2009
  *	
  *	edited by:  Phil Thompson
- *	@modified	24/04/2013
+ *	@modified	02/05/2013
  *
- *	document.ready
  */
 
 
@@ -28,20 +27,21 @@
         	
         	
         	$('#PrimaryContent').on('click', '.pagination a', function(e){
+        	
+        		
+				var	URL = $(this).attr("href"); // href value of clicked link
 	
 				e.preventDefault();
 				
-				// href value of clicked link
-				var URL = $(this).attr("href");
-				if(URL.indexOf('?mode=ajax') == -1 && URL.indexOf('?') == -1){
+				
+				if(URL.indexOf('?mode=ajax') === -1 && URL.indexOf('?') === -1){
 					URL = URL + '?mode=ajax';
-				} else if(URL.indexOf('?mode=ajax') == -1 && URL.indexOf('?') != -1){
+				} else if(URL.indexOf('?mode=ajax') === -1 && URL.indexOf('?') !== -1){
 					URL = URL + '&mode=ajax';
 				} 
-				
-				var hash = $(this).attr("href");
+
 				// Load in the new page
-				self.loadByAjax(URL, hash);
+				self.loadByAjax(URL);
 		
 			});
         	
@@ -54,14 +54,15 @@
 		 *	loadByAjax
 		 *	Use AJAX to grab new content and load it into place
 		 *	@param string URL 	/news/2009/06
-		 *	@param string hash	#page2
 		 */
-        loadByAjax: function(URL, hash){
+        loadByAjax: function(URL){
         
         	var self = this,
         		containerId = 'PrimaryContent',
 				$container = $('#' + containerId),
-				loader = '<div id="Loading">Loading new content&hellip;</div>';
+				loader = '<div id="Loading">Loading new content&hellip;</div>',
+				jqxhr,
+				ajaxData;
 			
 			if($container.find('.ajax-container').length === 0){
 				$container.wrapInner('<div class="ajax-container" />');
@@ -74,19 +75,19 @@
 			window.location.hash = self.urlHash(URL);
 		
 			// Make AJAX call to set URL then if we're successful show the new content else unhide the old stuff
-			var jqxhr = $.ajax({
+			jqxhr = $.ajax({
 				url: URL,
 				context: $container.find('.ajax-container')
 			})
 			.done(function(data) { 
 		
-				var ajaxData = $('<div>').html(data).find('#' + containerId).html();
+				ajaxData = $('<div>').html(data).find('#' + containerId).html();
 				$(this).html(ajaxData);
 				// Re-run function to allow future AJAX calls
-		  		if(typeof styliseCalendar == 'function'){
+		  		if(typeof styliseCalendar === 'function'){
 		  			styliseCalendar();
 		  		}
-		  		if(typeof autoSubmitOptions == 'function'){
+		  		if(typeof autoSubmitOptions === 'function'){
 		  			autoSubmitOptions();
 		  		}
 		
@@ -109,28 +110,30 @@
 		 */
         loadFromHash: function(){
         	var self = this,
-        		currentHash = window.location.hash;
-				currentHash = currentHash.replace('#','/'),
-				selectedHref = $('.pagination').find('.selected').find('a').attr('href');
+        		currentHash = window.location.hash.replace('#', '/'),
+				selectedHref = $('.pagination').find('.selected').find('a').attr('href'),
+				URLPartOne,
+				URL,
+				hash;
 			
 			if(currentHash.length > 0 && selectedHref.length > 0){
 		
 				// Does the selected href differ from the hash (to an extent)?
-				if(selectedHref.indexOf(currentHash) == -1){
+				if(selectedHref.indexOf(currentHash) === -1){
 				
 					// In the pagination the link to page 1 
 					// will always be /section/ so grab it
 					
-					var URLPartOne = $('.pagination li:nth-child(2) a').attr('href');
+					URLPartOne = $('.pagination').find('li:nth-child(2)').find('a').attr('href');
 					
 					// Now remove all the extra slashes
-					URLPartOne = URLPartOne.replace('/','');
-					currentHash = currentHash.replace('/','');
+					URLPartOne = URLPartOne.replace('/', '');
+					currentHash = currentHash.replace('/', '');
 					
 					// Rebuild the URL and hash and then 
 					// grab the required page
-					var URL = '/' + URLPartOne + currentHash.replace('/', '') + '?mode=ajax';
-					var hash = self.urlHash(selectedHref);
+					URL = '/' + URLPartOne + currentHash.replace('/', '') + '?mode=ajax';
+					hash = self.urlHash(selectedHref);
 					self.loadByAjax(URL);
 				}
 			}
@@ -146,7 +149,7 @@
         urlHash: function(href){
         	var hash = href.replace(href.split('/')[1] + '/', '');
 	
-			if(hash.indexOf('?') != -1) { 
+			if(hash.indexOf('?') !== -1) { 
 				hash = hash.split('?')[0]; 
 			}
 			
