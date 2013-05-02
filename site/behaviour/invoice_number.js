68f41f01f6ac998a69184bbd8521fc8340173067
Phil Thompson <hello@philthompson.co.uk>
2013-05-02 18:22:10 +0100

    JS clean-up

--- site/behaviour/invoice_number.js
+++ site/behaviour/invoice_number.js
@@ -15,19 +15,25 @@
  *	create an invoice number via AJAX (see /ajax/invoice_number.php)
  */
 function generateInvoiceNumber(){
+
+	var $refNumber = $('#internal_reference_number');
+	
+	if($refNumber.length === 0){
+		return;
+	}
 	
 	// Add button
-	$('#internal_reference_number').addClass('int').after('<a href="#" class="refresh button" id="invoice_button">Generate</a>');
+	$refNumber.addClass('int').after('<a href="#" class="refresh button" id="invoice_button">Generate</a>');
 	
 	// Get the invoice number with AJAX when user clicks the generate button
-	$('#invoice_button').click(function(){
+	$('#invoice_button').click(function(e){
 		
 		$(this).load('/ajax/invoice_number.php?mode=ajax', '', function(){
 			$(this).hide();
-			$('#internal_reference_number').val($(this).html());
+			$refNumber.val($(this).html());
 		});
 		
-		return false;
+		e.preventDefault();
 	});
 }
 
