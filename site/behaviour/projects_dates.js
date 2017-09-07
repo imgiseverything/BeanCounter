fd2c623a093bdc5396a4633b3974b02eee5c582e
Phil Thompson <hello@philthompson.co.uk>
2017-09-07 15:50:28 +0100

    Required payment date is now editable when the project has been invoiced (but not paid)
    There was a bug where the system never let you edit the payment required date menaign if you entered it wrong your projects could sya they were overdue when they weren’t with no way to fix it.

--- site/behaviour/projects_dates.js
+++ site/behaviour/projects_dates.js
@@ -8,45 +8,47 @@
  *	@copyright 2008 Phil Thompson
  *	@edited 24/04/2013 Phil Thompson
  */
- 
- 
+
+
 /* Hide/show date element depending on the project stage elements */
 $(document).ready(function(){
-	initProjectDates(); 	
+	initProjectDates();
 });
- 
+
 function initProjectDates(){
 
 	project_dates();
- 	
+
  	$('#project_stage').change(function(){
  		project_dates();
  	});
 }
- 
+
 /**
  *	project_dates()
  *	hide date fields unless they are required
  */
 function project_dates(){
- 
+
  	var projectStage = $('#project_stage').val();
- 	
+
  	$('fieldset.date').hide();
 
- 	if(projectStage.length > 0){ 
- 		
+ 	if(projectStage.length > 0){
+
+	 	projectStage = parseInt(projectStage, 10)
+
 	 	$('#payment_required_day').parent().hide();
-	 	
-	 	if(projectStage > 1){	
-	 		$('#payment_expected_day').parent().show();	 	
+
+	 	if(projectStage > 1){
+	 		$('#payment_expected_day').parent().show();
 	 	}
-	 	
+
 	 	/* STAGE: Invoiced */
 	 	if(projectStage === 3){
 	 		$('#payment_required_day').parent().show();
 	 	}
-	 	
+
  	}
- 	
+
 }
